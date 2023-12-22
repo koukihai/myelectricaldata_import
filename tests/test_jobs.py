bf6bc7465d38ecea6657cbcd0792f3cc8343356c
koukihai <koukihai@hotmail.com>
2023-12-22 23:23:37 +0100

    add logging observability

--- tests/test_jobs.py
+++ tests/test_jobs.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from contextlib import contextmanager
 from unittest import TestCase
@@ -44,15 +45,17 @@ class TestJob(TestCase):
 
     @mock.patch('models.jobs.Job.job_import_data')
     def test_boot(self, m: mock.Mock):
-        with setenv(DEV="true", DEBUG="true"):
+        with setenv(DEV="true", DEBUG="true"), self.assertLogs(logging.getLogger()) as logs:
             res = self.job.boot()
             self.assertFalse(res), "called with DEV or DEBUG should return False"
             self.assertEqual(0, m.call_count), "job_import_data should not be called"
+            self.assertEqual(['WARNING:root:=> Import job disable'], logs.output)
 
         m.return_value = {"status": "Mocked"}
-        res = self.job.boot()
-        self.assertEqual(m.return_value["status"], res)
-        m.assert_called_once()
+        with self.assertNoLogs(logging.getLogger()):
+            res = self.job.boot()
+            self.assertEqual(m.return_value["status"], res)
+            m.assert_called_once()
 
     @mock.patch('models.jobs.Job.export_influxdb')
     @mock.patch('models.jobs.Job.export_home_assistant_ws')
@@ -71,25 +74,30 @@ class TestJob(TestCase):
     @mock.patch('models.jobs.Job.get_tempo')
     @mock.patch('models.jobs.Job.get_gateway_status')
     def test_job_import_data(self, *args: mock.Mock):
-        res = self.job.job_import_data(target=None)
-        self.assertTrue(res["status"])
-        for m in args:
-            if m._mock_name in ["get_gateway_status", "get_tempo", "get_ecowatt"]:
-                m.assert_called_once()
-            else:
-                self.assertEqual(len(self.job.usage_points), m.call_count)
+        with self.assertLogs(logging.getLogger()) as logs:
+            res = self.job.job_import_data(target=None)
+            self.assertTrue(res["status"])
+            for m in args:
+                if m._mock_name in ["get_gateway_status", "get_tempo", "get_ecowatt"]:
+                    m.assert_called_once()
+                else:
+                    self.assertEqual(len(self.job.usage_points), m.call_count)
+            self.assertIn("INFO:root:DÉMARRAGE DU JOB D'IMPORTATION DANS 10S", logs.output)
 
     def test_header_generate(self):
         from dependencies import get_version
-
-        # self.header_generate() assumes self.usage_point_config is overwritten
-        for self.job.usage_point_config in [UsagePoints()]:
-            self.assertDictEqual(
-                {'Authorization': None, 'Content-Type': 'application/json', 'call-service': 'myelectricaldata',
-                 'version': get_version()},
-                self.job.header_generate())
+        with self.assertNoLogs(logging.getLogger()):
+            # self.header_generate() assumes self.usage_point_config is overwritten
+            for self.job.usage_point_config in [UsagePoints()]:
+                self.assertDictEqual(
+                    {'Authorization': None, 'Content-Type': 'application/json', 'call-service': 'myelectricaldata',
+                     'version': get_version()},
+                    self.job.header_generate())
 
     @mock.patch('models.jobs.Job.header_generate')
     def test_get_gateway_status(self, _):
-        res = self.job.get_gateway_status()
-        self.assertTrue(res["status"])
+        with self.assertLogs(logging.getLogger()) as logs:
+            res = self.job.get_gateway_status()
+            self.assertTrue(res["status"])
+            self.assertIn("INFO:root:RÉCUPÉRATION DU STATUT DE LA PASSERELLE :", logs.output)
+            self.assertIn("INFO:root:status: True", logs.output)
